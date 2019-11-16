530fe50495fec53a8d85e673d1c69e86b99cb995
Shivanshu mishra <shivanshu.mishra97@gmail.com>
2019-11-16 18:06:57 +0100

    Intialize Repository

---
+++ src/main/java/com/spring/documentdb/DocumentDbApplication.java
@@ -0,0 +1,13 @@
+package com.spring.documentdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/documentdb/DocumentDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.documentdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
